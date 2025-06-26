3b9fd93339264598f13801703a9e9fe15050b0bd
vivek vashishtha <vivek@viveks-MacBook-Air.local>
2025-06-26 11:22:25 +0530

    save git credientials

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,5 @@ import { userRegestration, userLogin } from '../controller/userController.js';
 const router = express.Router();
 
 router.post('/users/regestration', userRegestration);
-
   
 export default router;
